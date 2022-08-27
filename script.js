60c0e6eb5aef86a91f086def0b2fbd89e536f8aa
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-27 11:51:38 -0700

    fixed bug with local storage

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var moment = moment().format("H");
 console.log(moment)
-
 //localStorage.setItem("label","value");
 //localStorage.getItem("label")
 
 $(document).ready(function (){
-    moment(testDate).format('MM/DD/YYYY');
+    moment().format('MM/DD/YYYY');
     // Moment Date Stamp
+
+    $(".time").each(function(){
+        $(this).append("<button class='submitTime'>Submit</button>")
+    })
+
+    $(".submitTime").each(function(){
+        $(this).on("click",function(event){
+            let text = $(this).prev().val()
+            console.log(text)
+        })
+    })
     $("#saveButton").click(function(event){
         console.log(event.target.previousElementSibling.value)
         localStorage.setItem("text",event.target.previousElementSibling.value);  
